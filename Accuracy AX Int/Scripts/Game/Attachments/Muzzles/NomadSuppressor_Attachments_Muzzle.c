c12bdb6cba7f4a042e7e276e445b249ba03409ad
Luthendril <nickrhenen@gmail.com>
2025-12-18 15:57:09 +0100

    1.0.2
    Added: Magazine/Amo type: API en tracer
    Added: wapen prefab, wapen met demper
    Added: Olive Green, Urban Gray Camo
    Added: Zeroing voor 3-15x50 scope (accuraat tot 1000m alle ammo types 338lm)

---
+++ Accuracy AX Int/Scripts/Game/Attachments/Muzzles/NomadSuppressor_Attachments_Muzzle.c
@@ -0,0 +1,9 @@
+//////////////////// MUZZLE ATTACHMENTS PER CALIBER ////////////////////
+
+class AttachmentMuzzle338Class {}
+
+AttachmentMuzzle338Class AttachmentMuzzle338Source;
+
+class AttachmentMuzzle338 : AttachmentMuzzle
+{
+};
